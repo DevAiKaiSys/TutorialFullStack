c18577c6f95fe5beb8ad3e93dbe6a59d0a8a946d
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-06-06 17:10:38 +0700

    Auth Service & Calling Signup User API

--- CodeWithProjects/HotelManagementSystem/HotelWeb/src/app/auth/components/register/register.component.ts
+++ CodeWithProjects/HotelManagementSystem/HotelWeb/src/app/auth/components/register/register.component.ts
@@ -8,6 +8,9 @@ import {
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { AuthService } from '../../services/auth/auth.service';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -19,13 +22,29 @@ import { NzButtonModule } from 'ng-zorro-antd/button';
 export class RegisterComponent {
   registerForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private message: NzMessageService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.registerForm = this.fb.group({
       email: [null, [Validators.email, Validators.required]],
       password: [null, Validators.required],
       name: [null, Validators.required],
+    });
+  }
+
+  submitForm() {
+    this.authService.register(this.registerForm.value).subscribe((res) => {
+      if (res.id != null) {
+        this.message.success('Signup successful', { nzDuration: 5000 });
+        this.router.navigateByUrl('/');
+      } else {
+        this.message.error(`${res.message}`, { nzDuration: 5000 });
+      }
     });
   }
 }
